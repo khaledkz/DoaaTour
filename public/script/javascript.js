cd7aac1c732686cb400f54dfe0a5e8e0c717760a
kharazzoro <kha.1991.14.2017@gmail.com>
2017-10-22 20:40:33 +0100

    update work

---
+++ public/script/javascript.js
@@ -0,0 +1,15 @@
+let i=0;
+
+
+document.getElementById('menu').addEventListener("click",()=>{
+    i++;
+    if(i%2===0){
+        console.log("Hello World")
+        
+    }else{
+        console.log("Hello ppl")
+        
+    }
+})
+
+ 
