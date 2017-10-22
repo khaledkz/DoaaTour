4ea6d23af403aef7a2e382044ad6e51cd2e399d5
kharazzoro <kha.1991.14.2017@gmail.com>
2017-10-22 18:40:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+const exphbs = require("express-handlebars")
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+app.get('/about-us', (req, res) => {
+    res.render("about-us")
+})
+
+app.get('/contact-us', (req, res) => {
+    res.render("contact-us")
+})
+
+app.get('/umrah', (req, res) => {
+    res.render("umrah")
+})
+app.get('/hotel', (req, res) => {
+    res.render("hotel")
+})
+app.get('/haj', (req, res) => {
+    res.render("haj")
+})
+app.listen(3000);
